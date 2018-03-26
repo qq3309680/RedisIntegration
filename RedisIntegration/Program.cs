91b30cd0e2c7c071678d27a4c3c6662b761ff806
谭振 <tanz01@haid.com.cn>
2018-03-26 10:45:15 +0800

    v1.0.0

---
+++ RedisIntegration/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisIntegration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "tanzhen";
+            new RedisHelper().Set("abc", s, 10);
+            Console.WriteLine("ok");
+            Console.ReadKey();
+
+            Console.WriteLine(new RedisHelper().Get<string>("abc"));
+            //Console.WriteLine("ok");
+            Console.ReadKey();
+        }
+    }
+}
